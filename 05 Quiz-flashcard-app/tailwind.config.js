a042edbb745b7880ce6e9489807e54e69660e97c
Arin Gupta <aringupta2244@gmail.com>
2025-08-17 13:57:24 +0530

    Tailwind settings changes

--- 05 Quiz-flashcard-app/tailwind.config.js
+++ 05 Quiz-flashcard-app/tailwind.config.js
@@ -17,8 +17,8 @@ export default {
         },
       },
       animation: {
-        fadeIn: "fadeIn 1s ease-in-out",
-        fadeOut: "fadeOut 1s ease-in-out",
+        fadeIn: "fadeIn 0.7s ease-in-out",
+        fadeOut: "fadeOut 0.7s ease-in-out",
       },
     },
   },
